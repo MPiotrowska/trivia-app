2e32a377843c2c5f4b764f90699ea40e5248de33
MPiotrowska <monikapiotrowska27@gmail.com>
2020-10-28 18:46:57 +0100

    Radio Buttons logic

--- src/components/Question/Index.jsx
+++ src/components/Question/Index.jsx
@@ -1,36 +1,68 @@
 import React from "react";
 import { RadioButton } from "../RadioButton/Index.jsx";
-import { useQuestionState, useQuestionDispatch} from "../../lib/questionContext";
-
+import {
+  useQuestionState,
+  useQuestionDispatch,
+} from "../../lib/questionContext";
+import { shuffle } from "../../helpers/shuffle";
 
 export const Question = () => {
   const questionState = useQuestionState();
   const dispatch = useQuestionDispatch();
+  const correctAnswer = questionState.data[questionState.question].correct;
+  const [answer, setAnswer] = React.useState(false);
+
+  const [allAnswers, setAllAnswers] = React.useState([]);
+  const [selected, setSelected] = React.useState("");
+
+  const handleChange = (e) => {
+    setSelected(e.target.name);
+  };
+
+  const currentQuestion = questionState.data[questionState.question];
+
   const handleClick = () => {
-    if ( questionState.question === 10) {
-      dispatch({ type: 'gameOver'})
+    setSelected("")
+    if (questionState.question === 9) {
+      dispatch({ type: "gameOver" });
       return;
     }
 
     dispatch({
-      type:'nextQuestion'
-    })
-
-  }
+      type: "nextQuestion",
+    });
+  };
 
   React.useEffect(() => {
-    console.log("Question is number: ", questionState.question);
+    const allJoined = [...currentQuestion.incorrect, currentQuestion.correct];
+    const shuffled = shuffle(allJoined);
+    setAllAnswers(shuffled);
   }, [questionState.question]);
 
+  React.useEffect(() => {
+    setAnswer(correctAnswer === selected);
+  }, [answer]);
+
   return (
-    <div>
-      <p>{questionState.data[questionState.question].question}</p>
-      <form action="">
-        {questionState.data[questionState.question].incorrect.map((a) => {
-          return <RadioButton value={a} />;
-        })}
-      </form>
+    <>
+      <div>
+        <p>{currentQuestion.question}</p>
+        <form action="">
+          {allAnswers.map((a) => {
+            return (
+              <RadioButton
+                value={a}
+                checked={selected === a}
+                onChange={handleChange}
+              />
+            );
+          })}
+        </form>
+        {selected.length > 0 ? (
+          <p>Your answer is {answer === true ? "Correct" : "Not Correct"}</p>
+        ) : null}
+      </div>
       <button onClick={handleClick}>Next Question</button>
-    </div>
+    </>
   );
 };
--- src/components/RadioButton/Index.jsx
+++ src/components/RadioButton/Index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const RadioButton = ({value}) => {
+export const RadioButton = ({ value, checked= false, onChange }) => {
   return (
     <div>
-      <input type="radio" id={value} name={value} value={value} checked />
+      <input
+        type="radio"
+        id={value}
+        name={value}
+        value={value}
+        checked={checked}
+        onChange={onChange}
+      />
       <label for={value}>{value}</label>
     </div>
   );
