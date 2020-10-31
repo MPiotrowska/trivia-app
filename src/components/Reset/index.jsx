2d37f0a0a7f604bafec98ed88c35d42b65bd03a7
MPiotrowska <monikapiotrowska27@gmail.com>
2020-10-31 18:05:20 +0100

    add Reset component changes

--- src/components/Reset/index.jsx
+++ src/components/Reset/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { useQuestionState, useQuestionDispatch } from "../../lib/questionContext";
+import {
+  useQuestionState,
+  useQuestionDispatch,
+} from "../../lib/questionContext";
 import { Button } from "../Button";
+import Leo from "../../images/Leo.gif";
+import Bill from "../../images/Bill.gif";
+import "./Reset.css";
 
 export const Reset = () => {
   const dispatch = useQuestionDispatch();
@@ -17,10 +23,17 @@ export const Reset = () => {
 
   return (
     <>
-      <div>
-        <p>Your score is: {state.correctAnswersCounter} <span>/ 10</span> </p>
+      <div className="gameOverContainer">
+        <h1 className="welcomeText" style={{ fontWeight: "bold" }}>
+          Your score is: {state.correctAnswersCounter} <span>/ 10</span>{" "}
+        </h1>
+        {state.correctAnswersCounter > 5 ? (
+          <img className="gameOverImage" src={Leo} alt="" />
+        ) : (
+          <img className="gameOverImage" src={Bill} alt="" />
+        )}
       </div>
-      
+
       <Button onClick={handleClick}>Play again</Button>
     </>
   );
